01be3f04d8eaf92233658943f6a360671796b590
Кирилл Дудников <kirill.d.dudnikov@gmail.com>
2022-05-23 14:46:22 +0300

    Косметическое улучшение: теперь после завершения программы удаляется папка for_symlinks.

--- task13-1.c
+++ task13-1.c
@@ -1,12 +1,26 @@
+#define _XOPEN_SOURCE 500
+
 #include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/types.h>
 #include <sys/stat.h>
-#include <unistd.h>
 #include <string.h>
 
-// Директория for_symlinks автоматически не удаляется.
+#include <ftw.h>
+#include <unistd.h>
+
+// Директория for_symlinks автоматически удаляется.
+int unlink_cd(const char *fpath, const struct stat *sb, int typeflag, struct FTW *ftwbuf) {
+  int rv = remove(fpath);
+  if (rv)
+    perror(fpath);
+  return rv;
+}
+
+int rmrf(char *path) {
+  return nftw(path, unlink_cd, 64, FTW_DEPTH | FTW_PHYS);
+}
 
 int main()
 {
@@ -32,20 +46,27 @@ int main()
     sprintf(pathfrom, "for_symlinks/%d", depth+1);
     if (symlink(pathto, pathfrom) == -1) {
       printf("error on symlink, max rec depth=%d\n", depth);
-      exit(0);
+      break;
     }
     fd = open(pathfrom, O_RDONLY);
     readretval = read(fd, string, 3);
     close(fd);
     if (readretval == -1) {
       printf("error on read, max rec depth=%d\n", depth);
-      exit(0);
+      break;
     }
     if (strcmp(string, "ok") != 0) {
       printf("error on string in file, max rec depth=%d\n", depth);
-      exit(0);
+      break;
     }
     depth++;
   }
+
+  if (rmrf("for_symlinks") != -1) {
+    printf("for_symlinks dir removed correctly\n");
+  } else {
+    printf("error while removing dir for_symlinks\n");
+  }
+  
   return 0;
 }
